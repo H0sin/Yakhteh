93a143b954abf9fde0b2b7fd766bddaec02a3fec
ali <alibabadai1378@gmail.com>
2025-09-21 23:00:22 +0330

    feat: Update type declarations and add missing type definitions for React and MUI

--- frontend_yakhteh/src/types/global.d.ts
+++ frontend_yakhteh/src/types/global.d.ts
@@ -1,11 +1,2 @@
-declare module 'react';
-declare module 'react-dom/client';
-declare module 'react-router-dom';
-declare module 'react/jsx-runtime';
-declare module 'axios';
-declare module '@mui/material';
-declare module '@mui/material/*';
-declare module '@mui/icons-material/*';
-declare module '@emotion/react';
-declare module '@emotion/styled';
+/// <reference types="vite/client" />
 
